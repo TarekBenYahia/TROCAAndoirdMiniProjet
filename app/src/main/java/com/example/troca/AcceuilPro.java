9e343ed4c9c28252077587f405377a0b2f0be813
TarekBenYahia <60258461+TarekBenYahia@users.noreply.github.com>
2020-12-06 16:18:14 +0100

    clientId 06/12/2020

--- app/src/main/java/com/example/troca/AcceuilPro.java
+++ app/src/main/java/com/example/troca/AcceuilPro.java
@@ -30,7 +30,8 @@ public class AcceuilPro extends AppCompatActivity {
         if (drawer.isDrawerOpen(GravityCompat.START)){
             drawer.closeDrawer(GravityCompat.START);
         }
+        else {
+            super.onBackPressed();}
 
-        super.onBackPressed();
     }
 }
